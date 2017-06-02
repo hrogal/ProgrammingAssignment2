1636a48066004a4ba4cc5c5d1c54295841caa2e8
hrogal <henry.rogalin@tufts.edu>
2017-06-01 21:54:27 -0400

    Assignment file changed using instructions as template

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The goal of these functions is to return the inverse of a matrix, with the
+## assumption that it is invertable, only if this is the first time for this 
+## specific ooperation.
 
-## Write a short comment describing this function
+## The first funtion makes a global matrix object, and has functions for
+## getting and setting the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes a matrix object from makeCacheMatrix, and returns the
+## inverse matrix, solving for it if it is the first run, and returning the
+## saved (cached) result if it is a subsequent operation.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
